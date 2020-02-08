95675946d9a233c90caa75f51aa30c53ba0cf784
kzarama <kevin.zarama1@correo.icesi.edu.co>
2020-02-07 21:15:55 -0500

    fixing

--- no-injection/src/main/java/co/edu/icesi/ci/injectionexample1/repositories/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package co.edu.icesi.ci.injectionexample1.repositories;
-
-import co.edu.icesi.ci.injectionexample1.model.Student;
-
-public interface StudentRepository {
-
-	public Student getStudent (String id);
-}
