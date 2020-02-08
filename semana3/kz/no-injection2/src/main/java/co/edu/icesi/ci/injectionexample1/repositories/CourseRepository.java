0d2d6b530961847999ad3d67e48bf4a9eac7c446
kzarama <kevin.zarama1@correo.icesi.edu.co>
2020-02-07 20:58:11 -0500

    borrados antiguos proyectos debido a un error

--- semana3/kz/no-injection2/src/main/java/co/edu/icesi/ci/injectionexample1/repositories/CourseRepository.java
+++
@@ -1,8 +0,0 @@
-package co.edu.icesi.ci.injectionexample1.repositories;
-
-import co.edu.icesi.ci.injectionexample1.model.Course;
-
-public interface CourseRepository {
-
-	public Course getCourse (String id);
-}
--- semana3/kz/no-injection2/src/main/java/co/edu/icesi/ci/injectionexample1/service/RegistrationService.java
+++
@@ -1,6 +0,0 @@
-package co.edu.icesi.ci.injectionexample1.service;
-
-public interface RegistrationService {
-
-	public boolean enrolStudent(String studentId, String courseId, int semester);
-}
