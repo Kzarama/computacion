9e0c44c7b71e8de4e262bbaabd520a443c525d96
kzarama <kevin.zarama1@correo.icesi.edu.co>
2020-02-07 21:57:47 -0500

    second activity started

---
+++ LifeCycle/src/main/java/com/example/LifeCycle/LifeCycleApplication.java
@@ -0,0 +1,13 @@
+package com.example.LifeCycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeCycleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeCycleApplication.class, args);
+	}
+
+}
---
+++ LifeCycle/src/test/java/com/example/LifeCycle/LifeCycleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LifeCycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeCycleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
