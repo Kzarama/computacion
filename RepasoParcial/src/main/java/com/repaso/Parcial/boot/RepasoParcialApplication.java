31d6d48f7f4c2695179a831b0fb05c338729cda8
kzarama <kevin.zarama1@correo.icesi.edu.co>
2020-02-13 20:58:00 -0500

    initial tasks finished

--- RepasoParcial/src/main/java/com/repaso/Parcial/boot/RepasoParcialApplication.java
+++ RepasoParcial/src/main/java/com/repaso/Parcial/boot/RepasoParcialApplication.java
@@ -1,4 +1,4 @@
-package com.repaso.Parcial;
+package com.repaso.Parcial.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ RepasoParcial/src/main/java/com/repaso/Parcial/repositories/PersonRepositoryImp.java
@@ -0,0 +1,35 @@
+package com.repaso.Parcial.repositories;
+
+import java.util.HashMap;
+
+import com.repaso.Parcial.model.Person;
+
+public class PersonRepositoryImp implements PersonRepository {
+	
+	HashMap<String, Person> personHash;
+	
+	public PersonRepositoryImp() {
+		personHash = new HashMap<String, Person>();
+	}
+	
+	public boolean savePerson(String firstName, String lastName) {
+		Person newPerson = new Person(firstName, lastName);
+		personHash.put(lastName, newPerson);
+		return personHash.containsKey(lastName);
+	}
+	
+	public Person queryPerson(String lastName) {
+		return personHash.get(lastName);
+	}
+	
+	public boolean deletePerson(String lastName) {
+		return personHash.remove(lastName, personHash.get(lastName));
+	}
+	
+	public boolean updatePerson(String firstName, String lastName) {
+		personHash.remove(lastName);
+		personHash.put(lastName, new Person(firstName, lastName));
+		return true;
+	}
+	
+}
