48c6492b84348cf95935b11dedda35578ba2125f
kzarama <kevin.zarama1@correo.icesi.edu.co>
2020-02-15 09:40:51 -0500

    task 2 finished

--- LifeCycle/src/main/java/com/example/LifeCycle/LifeCycleApplication.java
+++ LifeCycle/src/main/java/com/example/LifeCycle/LifeCycleApplication.java
@@ -1,11 +1,22 @@
 package com.example.LifeCycle;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class LifeCycleApplication {
-
+	
+	private MyBeanY beanY1;
+	private MyBeanY beanY2;
+	
+	@Autowired
+	public LifeCycleApplication(MyBeanY beanY1, MyBeanY beanY2) {
+		this.beanY1 = beanY1;
+		this.beanY2 = beanY2;
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(LifeCycleApplication.class, args);
 	}
--- LifeCycle/src/main/java/com/example/LifeCycle/MyBeanY.java
+++ LifeCycle/src/main/java/com/example/LifeCycle/MyBeanY.java
@@ -1,10 +1,12 @@
 package com.example.LifeCycle;
 
 import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Lazy
 @Component
+@Scope("prototype")
 public class MyBeanY {
 
 }
