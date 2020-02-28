980844b648d0098be1ba6fd6dfb66e43170cbfcf
kzarama <kevin.zarama1@correo.icesi.edu.com>
2020-02-28 10:14:48 -0500

    idk

--- CI-und3-junit5-advanced-init-code/src/test/java/co/edu/icesi/fi/ci/junit5exercise/test/JUnitSpringTests.java
+++ CI-und3-junit5-advanced-init-code/src/test/java/co/edu/icesi/fi/ci/junit5exercise/test/JUnitSpringTests.java
@@ -9,18 +9,23 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Tag;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import co.edu.icesi.fi.ci.junit5exercise.Junit5AdvancedApplication;
 import co.edu.icesi.fi.ci.junit5exercise.model.Order;
 import co.edu.icesi.fi.ci.junit5exercise.service.OrderService;
 
 @SpringBootTest
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = Junit5AdvancedApplication.class)
 public class JUnitSpringTests {
 	
 	@Autowired
