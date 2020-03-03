30b66d639708355527ca5b6a41472aa3775e9c14
kzarama <kevin.zarama1@correo.icesi.edu.com>
2020-03-03 17:03:33 -0500

    mvc started

---
+++ ci-192-mvc-tutorial-ini/src/main/java/co/edu/icesi/ci/mvctutorial/controller/UserController.java
@@ -0,0 +1,19 @@
+package co.edu.icesi.ci.mvctutorial.controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import co.edu.icesi.ci.mvctutorial.model.User;
+import co.edu.icesi.ci.mvctutorial.service.UserService;
+
+@Controller
+public class UserController {
+
+}
