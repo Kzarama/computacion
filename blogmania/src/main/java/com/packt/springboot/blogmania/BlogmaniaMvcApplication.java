02725d79caa93600f940861ce8875348b18eeb72
kzarama <kevin.zarama1@correo.icesi.edu.co>
2020-03-08 11:08:54 -0500

    blogmaniaMVC added

---
+++ blogmania/src/main/java/com/packt/springboot/blogmania/BlogmaniaMvcApplication.java
@@ -0,0 +1,13 @@
+package com.packt.springboot.blogmania;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogmaniaMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogmaniaMvcApplication.class, args);
+	}
+
+}
